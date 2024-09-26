9974e9b11c0c2ec144125aaa73ae3270d0fe6929
brightchild200 <brightchild.riya@gmail.com>
2024-09-26 20:17:06 +0530

    Initial commit

---
+++ app/src/main/java/com/example/househuntingkt/RegisterActivity.kt
@@ -0,0 +1,51 @@
+package com.example.househuntingkt
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.example.househuntingkt.R.id.password
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.register)
+
+//        val backArrow = findViewById<ImageView>(R.id.backArrow)
+        val getStarted = findViewById<Button>(R.id.getStarted)
+        val login = findViewById<TextView>(R.id.login)
+
+        val name = findViewById<EditText>(R.id.name)
+        val mobile = findViewById<EditText>(R.id.phone)
+        val email = findViewById<EditText>(R.id.email)
+        val password = findViewById<EditText>(password)
+
+        // Back to MainActivity (Home)
+
+
+        // Handle Get Started button click
+        getStarted.setOnClickListener {
+            val name = name.text.toString().trim()
+            val mobile = mobile.text.toString().trim()
+            val email = email.text.toString().trim()
+            val password = password.text.toString().trim()
+
+            if (name.isEmpty() || mobile.isEmpty() || email.isEmpty() || password.isEmpty()) {
+                Toast.makeText(this, "Please fill all fields", Toast.LENGTH_SHORT).show()
+            } else {
+                // You can proceed with form submission logic here
+                Toast.makeText(this, "Registration Successful", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+        // Redirect to LoginActivity
+        login.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
