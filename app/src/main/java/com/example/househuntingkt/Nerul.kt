c9d4b80ba525a71739981f46ec557423a21099d8
brightchild200 <brightchild.riya@gmail.com>
2024-10-17 12:41:15 +0530

    ladybug

--- app/src/main/java/com/example/househuntingkt/Nerul.kt
+++ app/src/main/java/com/example/househuntingkt/Nerul.kt
@@ -12,22 +12,22 @@ package com.example.househuntingkt
             setContentView(R.layout.nerul)
 
             // Find ImageViews by ID
-            val propertyImage1 = findViewById<ImageView>(R.id.nerulimg1)
-            val propertyImage2 = findViewById<ImageView>(R.id.nerulimg2)
+//            val propertyImage1 = findViewById<ImageView>(R.id.nerulimg1)
+//            val propertyImage2 = findViewById<ImageView>(R.id.nerulimg2)
 //            val propertyImage3 = findViewById<ImageView>(R.id.nerulim3)
 //            val propertyImage4 = findViewById<ImageView>(R.id.property_image4)
 
             // Set click listeners for each image
-            propertyImage1.setOnClickListener {
-                val intent = Intent(this, Nerul1::class.java)
-                startActivity(intent)
-            }
-
-            propertyImage2.setOnClickListener {
-                val intent = Intent(this, Nerul1::class.java)
-                startActivity(intent)
-            }
+//            propertyImage1.setOnClickListener {
+//                val intent = Intent(this, Nerul1::class.java)
+//                startActivity(intent)
+//            }
 //
+//            propertyImage2.setOnClickListener {
+//                val intent = Intent(this, Nerul1::class.java)
+//                startActivity(intent)
+//            }
+////
 //            propertyImage3.setOnClickListener {
 //                val intent = Intent(this, StMarryHousingSocietyActivity::class.java)
 //                startActivity(intent)
--- app/src/main/java/com/example/househuntingkt/Vashi.kt
+++ app/src/main/java/com/example/househuntingkt/Vashi.kt
@@ -11,22 +11,22 @@ class Vashi : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.vashi)
 
-        // Find ImageViews by ID
-        val vashiimg1 = findViewById<ImageView>(R.id.vashiImg1)
-        val vashiimg2 = findViewById<ImageView>(R.id.vashiimg2)
+//        // Find ImageViews by ID
+//        val vashiimg1 = findViewById<ImageView>(R.id.vashiImg1)
+//        val vashiimg2 = findViewById<ImageView>(R.id.vashiimg2)
 //            val propertyImage3 = findViewById<ImageView>(R.id.nerulim3)
 //            val propertyImage4 = findViewById<ImageView>(R.id.property_image4)
-
-        // Set click listeners for each image
-        vashiimg1.setOnClickListener {
-            val intent = Intent(this, Vashi1::class.java)
-            startActivity(intent)
-        }
-
-        vashiimg2.setOnClickListener {
-            val intent = Intent(this, Vashi1::class.java)
-            startActivity(intent)
-        }
+//
+//        // Set click listeners for each image
+//        vashiimg1.setOnClickListener {
+//            val intent = Intent(this, Vashi1::class.java)
+//            startActivity(intent)
+//        }
+//
+//        vashiimg2.setOnClickListener {
+//            val intent = Intent(this, Vashi1::class.java)
+//            startActivity(intent)
+//        }
 //
 //            propertyImage3.setOnClickListener {
 //                val intent = Intent(this, StMarryHousingSocietyActivity::class.java)
